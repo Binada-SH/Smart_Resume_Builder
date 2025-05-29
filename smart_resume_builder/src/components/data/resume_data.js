3fee55aa892403c96bbfedc0c5210f917051e615
Binada-SH <binadasasmika@gmail.com>
2025-05-29 12:14:28 +0530

    Data structure creation

---
+++ smart_resume_builder/src/components/data/resume_data.js
@@ -0,0 +1,30 @@
+export const resume_data = {
+
+    personal_info : {
+        full_name : "",
+        email : "",
+        cellular_number : "",
+        avatar : "",
+        linked_in : "",
+    },
+
+    education : {
+        school_name : "",
+        degree : "",
+        start_date : "",
+        end_date : "",
+        results : "",
+    },
+
+    skills : [],
+
+    projects : [
+        {
+            project_name :  "",
+            description:  "",
+            start_date : "",
+            end_date : "",
+            link : "",
+        }
+    ],
+}
